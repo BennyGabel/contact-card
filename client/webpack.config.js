f4063f314ff52534c3835831e5b50ebf505c46d9
13 Miles <bennygabel@gmail.com>
2022-12-09 10:21:27 -0500

    final lesson's code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,37 +1,66 @@
-// const path = require('path');
-//   module.exports = {
-//     mode: 'development',
-//     entry: './src/js/index.js',
-//     output: {
-//       // filename: 'main.js',
-//       filename: 'bundle.js',
-//       path: path.resolve(__dirname, 'dist'),
-//     },
-//   };
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-//         type: 'asset/resource',
-//       },
-//     ]
-//   }
-  
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require('path');
 
-const path = require("path");
 module.exports = {
-  mode: "development",
-  entry: "./src/js/index.js",
+  mode: 'production',
+  entry: './src/js/index.js',
   output: {
-    // filename: 'main.js',
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+    }),
+    new WebpackPwaManifest({
+        name: 'Contact Cards Application',
+        short_name: 'Contact Cards',
+        description: 'Keep track of important contacts!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/icon-manifest.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+          {
+            src: path.resolve('src/images/icon-manifest.png'),
+            size: '1024x1024',
+            destination: path.join('assets', 'icons'),
+            purpose: 'maskable'
+          }
+        ],
+      }), 
+      new InjectManifest({
+        swSrc: './sw.js',
+        swDest: 'service-worker.js',
+      }), 
+    ],
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
